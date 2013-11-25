20d61a6d7395b1e99f184909c5c9756f39d44609
mkyriacou <strattica@gmail.com>
2013-11-25 10:22:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreetBoss::Application.config.secret_token = '8ef26eb632c4e12f2c80c05bdb01e08c93b75de110e185a8e93653ede4d160bdda32f424f7b403ff440441142cc50fe0b06a377bb81010a18425836d9ab17b91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StreetBoss::Application.config.session_store :cookie_store, key: '_street-boss_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StreetBoss::Application.config.session_store :active_record_store
